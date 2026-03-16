16626318c09e3f768ef6223036e11bdb96360cb0
ankushoffice-commits <ankushoffice10@gmail.com>
2026-03-16 15:07:05 +0530

    Create indexOfFirstOccurence.java

---
+++ src/indexOfFirstOccurence.java
@@ -0,0 +1,9 @@
+class indexOfFirstOccurence {
+    public static int strStr(String haystack, String needle) {
+            return haystack.indexOf(needle);
+    }
+    
+    public static void main(String[] args){
+        System.out.println(strStr("sadbutsad","sad"));
+    }
+}
